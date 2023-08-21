98e3c0b3b88e1b6189b39ec4d8af38d7ae68f313
Ben North <ben@redfrontdoor.org>
2023-08-21 08:58:52 +0100

    (Whitespace)

--- src/lib/browser-devices-manager/substrates/ble.ts
+++ src/lib/browser-devices-manager/substrates/ble.ts
@@ -259,6 +259,7 @@ export class BleDeviceDriver implements BrowserDeviceDriver {
       return null;
     }
   }
+
   _notImplementedError(methodName: string) {
     return new Error(`BleDeviceDriver.${methodName}(): not implemented`);
   }
