b7bb2f1c2b370ce52e16cc94be8a98346f6d1552
Ben North <ben@redfrontdoor.org>
2023-09-11 16:05:17 +0100

    Update example build

--- browser-device-lib-example/browser-device-lib/index.js
+++ browser-device-lib-example/browser-device-lib/index.js
@@ -425,27 +425,6 @@ class BleHandledDevice extends BrowserHandledDevice {
         throw new Error("BleHandledDevice.acceptCharacteristicValue(): not implemented");
     }
 }
-const firstConnectableDevice = (devices, timeoutSeconds) => new Promise((resolve, reject) => {
-    const abortController = new AbortController();
-    const abortSignal = abortController.signal;
-    const onAdvert = (evt) => {
-        console.log("about to resolve from advert event:", evt);
-        abortController.abort();
-        resolve(evt.device);
-    };
-    const onTimeout = () => {
-        abortController.abort();
-        reject(new Error("no device advertisements received in time"));
-    };
-    setTimeout(onTimeout, timeoutSeconds * 1000.0);
-    devices.forEach((device) => {
-        console.log("watching adverts for", device);
-        device.watchAdvertisements({ signal: abortSignal }).then(() => {
-            console.log("adding event-listener for", device);
-            device.addEventListener("advertisementreceived", onAdvert);
-        });
-    });
-});
 class BleDeviceDriver {
     ////////////////////////////////////////////////////////////////////////
     // Concrete device drivers must implement the following.
@@ -548,47 +527,17 @@ class BleDeviceDriver {
         await this.connectCharacteristicValueListeners(handledDevice);
         return handledDevice;
     }
-    async tryProvideDevice(manager, specifier) {
+    async tryProvideDevice(_manager, specifier) {
         console.log("BLE tryProvideDevice()", specifier);
         if (!this.canProvide(specifier)) {
             console.log("can't provide");
             return null;
         }
-        // TODO: Query navigator.bluetooth.getDevices() and see if there's
-        // one in there which satisfies the specifier and we can connect to.
-        const allPermittedDevices = await navigator.bluetooth.getDevices();
-        console.log("BLE devices", allPermittedDevices);
-        let candidateDevices = [];
-        for (const device of allPermittedDevices) {
-            const leaseHolder = manager.leaseHolder(device);
-            console.log("device", device, "has leaseHolder", leaseHolder);
-            if (leaseHolder != null) {
-                // Not available; someone (maybe the requesting session, or
-                // maybe another session) under this manager already has a
-                // lease.
-                console.log("existing lease on", device);
-                continue;
-            }
-            if (!this.canHandleDevice(device, specifier)) {
-                console.log("can't handle", device);
-                // Unsuitable device for this driver.
-                continue;
-            }
-            candidateDevices.push(device);
-        }
-        console.log("Candidate BLE devices", candidateDevices);
-        if (candidateDevices.length > 0) {
-            try {
-                console.log("have candidates:", candidateDevices);
-                const device = await firstConnectableDevice(candidateDevices, 10.0);
-                console.log("attempting to createHandledDevice() for", device);
-                return await this.createHandledDevice(device);
-            }
-            catch (e) {
-                console.log("failed to construct handled device", e);
-                return null;
-            }
-        }
+        // We did experiment with using navigator.bluetooth.getDevices() to
+        // find a device the user has already permitted us to access, and
+        // then connect to that device.  However, this did not work
+        // reliably, so for now we are accepting that the user will
+        // encounter the pop-up permissions dialog when using a BT device.
         while (true) {
             console.log("attempting requestDevice()");
             // Accept devices which either provide the right services or have
