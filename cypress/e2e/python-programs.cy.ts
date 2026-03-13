dbc44c38d9f0b13373005ae614e00bd5dcd561ed
Ben North <ben@redfrontdoor.org>
2026-03-13 11:55:40 +0000

    Centroid(): Add e2e test

--- cypress/e2e/python-programs.cy.ts
+++ cypress/e2e/python-programs.cy.ts
@@ -655,6 +655,17 @@ describe("Runs valid Python programs", optsNoIsolation, () => {
       `,
       expOutputs: ["y=1 (1, 1)"],
     },
+    {
+      label: "Centroid(polygon)",
+      code: `
+        p1 = Point(-4, -4)
+        p2 = Point(-2, -4)
+        p = Polygon(p1, p2, 4)
+        p3 = Centroid(p)
+        print("Centroid", p3)
+      `,
+      expOutputs: ["Centroid (-3, -3)"],
+    },
   ];
 
   runsWithoutErrorSpecs.forEach((spec, specIdx) => {
