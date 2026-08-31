bd31bf9f2915ebe27296e0c4100c5f08893eda23
Patrick Prömer <patrick.proemer@geogebra.org>
2026-08-31 14:49:27 +0200

    INF-414: added eslint plugin security

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,20 +2,20 @@ import eslintReact from "@eslint-react/eslint-plugin";
 import eslintJs from "@eslint/js";
 import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
-
+import pluginSecurity from "eslint-plugin-security";
 export default defineConfig({
   files: ["**/*.ts", "**/*.mts", "**/*.tsx"],
-
   // Extend recommended rule sets from:
   // 1. ESLint JS's recommended rules
   // 2. TypeScript ESLint recommended rules
   // 3. ESLint React's recommended-typescript rules
+  // 4. eslint-plugin-security's recommended rules
   extends: [
     eslintJs.configs.recommended,
     tseslint.configs.recommended,
     eslintReact.configs["recommended-typescript"],
+    pluginSecurity.configs.recommended,
   ],
-
   // Configure language/parsing options
   languageOptions: {
     // Use TypeScript ESLint parser for TypeScript files
@@ -26,7 +26,6 @@ export default defineConfig({
       tsconfigRootDir: import.meta.dirname,
     },
   },
-
   // Custom rule overrides (modify rule levels or disable rules)
   rules: {
     "@eslint-react/no-missing-key": "warn",
@@ -35,5 +34,6 @@ export default defineConfig({
     "prefer-const": 0,
     // Allow suitably-named args to be unused.
     "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "security/detect-object-injection": "off",
   },
 });
